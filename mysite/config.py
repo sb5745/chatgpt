bdcf25d0cabfa077a3bde51e432c73dbb0e056ad
sb5745 <160140226+sb5745@users.noreply.github.com>
2024-10-11 17:38:07 +0900

    Update config.py

--- mysite/config.py
+++ mysite/config.py
@@ -1,3 +1,3 @@
 # config.py
-OPENAI_API_KEY = 'sk-proj-oKY5eCPqWBfF68VVS1zwl1leH67ihxa7ZmM_p_SqkW4RLYFtkWepiTqHKgT3BlbkFJFm3WTNCzpupIxcqYqn1AtN8GhRkTb8PO-B0dENdSqCydfATwNd69cgMb'
+OPENAI_API_KEY = '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'
 DEFAULT_MODEL = "gpt-4-turbo"
